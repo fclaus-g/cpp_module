bc556a77aafa39b0c46400bc0d5987e039a3b74c
fclaus-g <fclaus-g@student.42malaga.com>
2024-09-18 10:18:57 +0200

    test: new tests for executeForm()

--- cpp_module05/ex02/main.cpp
+++ cpp_module05/ex02/main.cpp
@@ -107,6 +107,11 @@ int	main()
 	//std::cout << YEL << "Bad Form test" << RES << std::endl;
 	//AForm *badForm = new AForm("badForm", 0, 0);
 
+	//Bureacrat executeForm test
+	std::cout << YEL << "Bureacrat executeForm test" << RES << std::endl;
+	hight.executeForm(form);
+	lowRobot.executeForm(robot);
+
 
 	std::cout << GRN << "\n Starts Destruction" << RES << std::endl;
 
