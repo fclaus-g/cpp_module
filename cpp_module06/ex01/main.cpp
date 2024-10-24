97d55a274daee67b2db815e0f29d92f73619e64f
fclaus-g <fclaus-g@student.42malaga.com>
2024-10-24 19:23:54 +0200

    feat: m06:ex01 add subject on main

--- cpp_module06/ex01/main.cpp
+++ cpp_module06/ex01/main.cpp
@@ -1,21 +1,21 @@
 
-/*Escriba una clase ScalarConverter que contenga solo un método estático "convert"
-que tome como parámetro una representación en cadena de un literal de C++ en su forma más común y muestre su valor en la siguiente serie de tipos escalares:
-• char
-• int
-• float
-• double
-Como esta clase no necesita almacenar nada en absoluto, los usuarios no deben poder instanciarla.
+/*Implementa una clase Serializer, que no será inicializable por el usuario de ninguna manera, con los siguientes métodos estáticos:
+```cpp
+uintptr_t serialize(Data* ptr);
+```
+Toma un puntero y lo convierte al tipo entero sin signo uintptr_t.
+```cpp
+Data* deserialize(uintptr_t raw);
+```
+Toma un parámetro entero sin signo y lo convierte en un puntero a Data.
 
-A excepción de los parámetros char, solo se utilizará la notación decimal.
-Ejemplos de literales char: 'c', 'a', ...
-Para simplificar las cosas, tenga en cuenta que los caracteres no visibles no deben usarse como
-entradas. Si una conversión a char no se puede mostrar, imprime un mensaje informativo.
-Ejemplos de literales int: 0, -42, 42...
-Ejemplos de literales float: 0.0f, -4.2f, 4.2f...
-También tienes que manejar estos pseudoliterales (ya sabes, por ciencia): -inff, +inff y nanf.
-Ejemplos de literales double: 0.0, -4.2, 4.2...
-También tienes que manejar estos pseudoliterales (ya sabes, por diversión): -inf, +inf y nan.*/
+Escribe un programa para probar que tu clase funciona como se espera.
+
+Debes crear una estructura de datos no vacía (es decir, que tenga miembros de datos).
+
+Usa serialize() en la dirección del objeto Data y pasa su valor de retorno a deserialize(). Luego, asegúrate de que el valor de retorno de deserialize() sea igual al puntero original.
+
+No olvides entregar los archivos de tu estructura de datos.*/
 
 #include "ScalarConverter.hpp"
 
